0a255ae391af07ec51e1ae433413b6488e1998a3
fishermanymc <fishermanymc@gmail.com>
2017-12-01 11:51:36 -0800

    revised addWorkerIP to accept both source and destination names.

--- addWorkerIP.cpp
+++ addWorkerIP.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 void addWorkerIP(char * IPFileName, char * hostFileName){
 	ifstream ipFile;
 	ipFile.open(IPFileName);
+	ifstream host;
+	host.open(hostFileName);
 	string myLine;
+	getline(host, myLine);
+	cout << myLine << endl;
+	host.close();
 	std::ofstream hostFile(hostFileName, ios::app);
 	hostFile << endl;
 	int numWorkers = 1;
@@ -19,5 +25,5 @@ void addWorkerIP(char * IPFileName, char * hostFileName){
 
 int main(int argc, char *argv[]){
 	addWorkerIP(argv[1], argv[2]);
-	printf("Worker IPs added");
+	printf("Worker IPs added\n");
 }
