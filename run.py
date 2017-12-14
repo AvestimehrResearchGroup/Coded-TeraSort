0736870dabece0a170c8abc3d78174a2347e12c4
fishermanymc <fishermanymc@gmail.com>
2017-12-15 10:35:48 +1100

    added a simple python script to call Master-Detection.sh

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+mode = "coded"
+IP = "./IPaddress.txt"
+data = "./Input/data.txt"
+os.system("./test.sh " + mode + " " + IP + " " + data)
+# os.system("./Master-Detection.sh " + mode + " " + IP + " " + data)
