a96a8a272afa39c744898e9ca077d1c647ef94fb
fishermanymc <fishermanymc@gmail.com>
2017-12-22 08:55:02 +1100

    revised teraSort.py for the fake user based on the test.

--- fake_master/teraSort.py
+++ fake_master/teraSort.py
@@ -1,62 +1,46 @@
 import os
 import time
-import subprocess
-import pipes
 
 
 def task(onefile, pathin, pathout):
-    realMasterIP = "192.168.0.0"
+    realMasterIP = "10.244.9.145"
     outName = "a1"
     # send the input file to the real master
+    print("Send file")
     os.system("sshpass -p 'zhifeng' scp -o StrictHostKeyChecking=no " +
-              pathin + onefile + "root@" +
+              pathin + onefile + " root@" +
               realMasterIP + ":/root/TeraSort/Input/data.txt")
 
     # execute uncoded TeraSort
+    print("Run code")
     os.system("sshpass -p 'zhifeng' ssh " + realMasterIP +
               " '/root/TeraSort/Master-Detection.sh uncoded'")
 
-    # wait until the result is avaiable
-    while not exists_remote(realMasterIP, "/root/TeraSort/Output/result.txt"):
-        time.sleep(1)
-
-    # # execute coded TeraSort
-    # os.system("sshpass -p 'zhifeng' ssh " + realMasterIP +
-    #           "bash /root/Terasort/Master-Detecion.sh coded")
-
-    # # wait until the result is avaiable
-    # while not exists_remote("root@" + realMasterIP,
-    #                         "/root/TeraSort/Output/result-C.txt"):
-    #     time.sleep(1)
-
     # download the results
-    os.system("sshpass -p 'zhifeng' scp -o StrictHostKeyChecking=no root@" +
-              realMasterIP + " /root/TeraSort/Output/result.txt" + ' ' +
-              pathout + outName + ".txt")
-    # os.system("sshpass -p 'zhifeng' scp root@" + realMasterIP +
-    #           "/root/teraSort/Output/result-C.txt" +
-    #           pathout + outName + "-C.txt")
+    print("Download the result")
+    while True:
+        try:
+            print("Downlao file")
+            os.system("sshpass -p 'zhifeng' scp -o" +
+                      " StrictHostKeyChecking=no root@" +
+                      realMasterIP +
+                      ":/root/TeraSort/Output/result.txt " +
+                      pathout + outName + ".txt")
+            break
+        except Exception as e:
+            print("waiting for output")
+            time.sleep(1)
 
     # remove the results from real master
-    os.system("sshpass -p 'zhifeng' ssh " + realMasterIP +
-              "rm /root/Terasort/Output/result.txt")
-    # os.system("sshpass -p 'zhifeng' ssh " + realMasterIP +
-    #           "rm /root/Terasort/Output/result.txt")
-
-
-def exists_remote(host, path):
-    """Test if a file exists at path on a host accessible with SSH."""
-    status = subprocess.call(
-        ['sshpass', '-p', 'zhifeng',
-         'ssh', host, 'test -f {}'.format(pipes.quote(path))])
-    if status == 0:
-        return True
-    if status == 1:
-        return False
+    print("Delete rermote files")
+    os.system("sshpass -p 'zhifeng' ssh -o StrictHostKeyChecking=no " +
+              realMasterIP +
+              " 'rm /root/TeraSort/Output/result.txt'")
+    print("Finishsed")
 
 
 if __name__ == '__main__':
     oneName = 'data.txt'
-    pathin = './Input'
-    pathout = './Output'
+    pathin = './'
+    pathout = './'
     task(oneName, pathin, pathout)
